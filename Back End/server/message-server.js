6a92983af89ede0f536c438885fdae696288ba0c
Ezat <ezatelbery164@gmail.com>
2025-05-08 14:38:58 +0300

    add file message server

--- Back End/server/message-server.js
+++ Back End/server/message-server.js
@@ -1,4 +1,5 @@
 const Message = require("../models/messageModels");
+const User = require("../models/userModels");
 const { asyncErrorHandler } = require("express-error-catcher");
 
 const createMessage = asyncErrorHandler(async (req, res, next) => {
@@ -36,8 +37,52 @@ const getConversation = asyncErrorHandler(async (req, res, next) => {
   res.status(200).json({ status: "success", data: messages });
 });
 
+const getAllConversationToAdmin = asyncErrorHandler(async (req, res, next) => {
+  const getAllMessage = await Message.find({ receiver: req.user._id });
+
+  console.log(getAllMessage);
+
+  let messagesToAdmin = [];
+
+  getAllMessage.map((message, i) => {
+    const findMessage = messagesToAdmin.findIndex(
+      (m) => m?.user._id?.toString() === message.sender._id.toString()
+    );
+
+    if (findMessage === -1) {
+      messagesToAdmin.push({
+        user: message.sender,
+        notRead: message.is_read ? 0 : 1,
+      });
+    } else {
+      if (!message.is_read) {
+        messagesToAdmin[findMessage].notRead += 1;
+      }
+    }
+  });
+
+  res.status(200).json({ status: "success", data: messagesToAdmin });
+});
+
+const readMessage = asyncErrorHandler(async (req, res, next) => {
+  const { sender, receiver } = req.body;
+
+  const messages = await Message.updateMany(
+    { sender, receiver },
+    {
+      $set: { is_read: true },
+    }
+  );
+
+  const getMessage = await Message.find({ sender, receiver });
+
+  res.status(200).json({ status: "success update", data: getMessage });
+});
+
 module.exports = {
   createMessage,
   getMessage,
   getConversation,
+  getAllConversationToAdmin,
+  readMessage,
 };
