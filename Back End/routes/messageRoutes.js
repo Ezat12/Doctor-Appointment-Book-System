b2c313ff435e26a5e37f8b7b080437493fbd8a52
Ezat <ezatelbery164@gmail.com>
2025-05-08 14:39:38 +0300

    add file message route

--- Back End/routes/messageRoutes.js
+++ Back End/routes/messageRoutes.js
@@ -1,13 +1,22 @@
 const express = require("express");
-const { protectAuth } = require("../server/auth-server");
+const { protectAuth, allowedTo } = require("../server/auth-server");
 const {
   getMessage,
   createMessage,
   getConversation,
+  getAllConversationToAdmin,
+  readMessage,
 } = require("../server/message-server");
 const router = express.Router();
 
 router.post("/", protectAuth, createMessage);
+router.get(
+  "/getAllConversationToAdmin",
+  protectAuth,
+  allowedTo("admin"),
+  getAllConversationToAdmin
+);
+router.put("/readMessage", protectAuth, readMessage);
 router.get("/:userId", protectAuth, getMessage);
 router.get("/conversation/:userId1/:userId2", protectAuth, getConversation);
 
