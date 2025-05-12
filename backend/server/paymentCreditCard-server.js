9e88ff7e7af86997756271aea7d40523a1e2948e
Ezat <ezatelbery164@gmail.com>
2025-05-12 15:58:37 +0300

    add change

--- backend/server/paymentCreditCard-server.js
+++ backend/server/paymentCreditCard-server.js
@@ -1,6 +1,9 @@
 const { asyncErrorHandler } = require("express-error-catcher");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KRY);
 const Appointment = require("../models/appointmentModels");
+const User = require("../models/userModels");
+const { sendNotification } = require("./notification-server");
+const sendEmail = require("../utils/sendEmail");
 
 const checkOutSession = asyncErrorHandler(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
@@ -20,11 +23,44 @@ const checkOutSession = asyncErrorHandler(async (req, res, next) => {
     success_url: "http://localhost:5173/my-appointment",
     cancel_url: "http://localhost:5173/my-appointment",
     customer_email: req.user.email,
-    client_reference_id: req.body.doctorId,
+    client_reference_id: req.body.appointmentId,
   });
 
   res.status(200).json({ status: "success", session });
 });
+
+const paidAppointment = async (session) => {
+  const appointment = await Appointment.findByIdAndUpdate(
+    session.client_reference_id,
+    { is_paid: true },
+    { new: true }
+  );
+
+  const admin = await User.findOne({ role: admin });
+  const io = req.app.get("io");
+  const connectedUsers = req.app.get("connectedUsers");
+
+  await sendNotification(
+    io,
+    connectedUsers,
+    admin._id,
+    "patient_payment",
+    `Payment confirmed for patient ${appointment.user.name} with amount ${
+      session.amount_total
+    } for appointment with Dr. ${
+      appointment.doctor.name
+    } on ${new Date().toLocaleDateString()}`
+  );
+
+  await sendEmail(
+    "payment_confirmed",
+    appointment.user.name,
+    appointment.doctor.name,
+    admin.email,
+    new Date().toLocaleDateString(),
+    appointment.doctor.appointmentFee
+  );
+};
 
 const webhookCheckout = asyncErrorHandler(async (req, res, next) => {
   const body = req.body;
@@ -45,14 +81,9 @@ const webhookCheckout = asyncErrorHandler(async (req, res, next) => {
   if (event.type === "checkout.session.completed") {
     console.log("Yes Complete");
 
-    const appointment = await Appointment.findByIdAndUpdate(
-      event.data.object.client_reference_id,
-      { is_paid: true },
-      { new: true }
-    );
+    const session = event.data.object;
 
-    console.log(appointment);
-
+    await paidAppointment(session);
     res.status(200).json({ status: "success" });
   } else {
     console.log("Back");
